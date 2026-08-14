e9e84c8c2adda59c6cb519bf1e2cd6c4ea66e2e1
dujavi <dujavi@gmail.com>
2026-08-14 11:23:59 -0500

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.ts
@@ -0,0 +1,35 @@
+class MinStack {
+    stack: number[] = new Array()
+
+    constructor() {}
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1]
+        return this.stack.slice(-1)[0]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return Math.min(...this.stack)
+    }
+}
